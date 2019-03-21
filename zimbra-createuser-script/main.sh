0fdcbee726084ae8d620aac91f9a31cb803ab51e
Vlad Korkach <Archonr94@gmail.com>
2019-03-21 12:08:07 +0200

    Update4

--- zimbra-createuser-script/main.sh
+++ zimbra-createuser-script/main.sh
@@ -5,7 +5,7 @@ file=./createuser.csv
 while IFS=, read email first last initials desc
 do
     IFS=@ read USER DOMAIN <<< $email
-    sudo -u zimbra /opt/zimbra/bin/zmprov ca $email 12345678 zimbraMailHost mail.itftc.com
+    sudo -u zimbra /opt/zimbra/bin/zmprov ca $email 12345678 zimbraMailHost "hostname"
     sudo -u zimbra /opt/zimbra/bin/zmprov ma $email zimbraCOSid "$sign"
     sudo -u zimbra /opt/zimbra/bin/zmprov ma $email givenName "$last"
     sudo -u zimbra /opt/zimbra/bin/zmprov ma $email sn "$first"
